3be33675f675616a40979c1b7e3781ebaf9d5a4c
Gleison Lima <gleisonlima@id.uff.br>
2021-04-11 20:32:59 -0300

    cadatrarIdeia_semCSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarIdeiaComponent } from './pages/cadastrar-ideia/cadastrar-ideia.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path:"home", component: DashboardComponent},
-  {path:"login", component: LoginComponent}
+  {path:"login", component: LoginComponent},
+  {path:"cadastrar-ideia", component: CadastrarIdeiaComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastrarIdeiaComponent } from './pages/cadastrar-ideia/cadastrar-ideia.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LoginComponent } from './pages/login/login.component';
     HeaderComponent,
     DashboardComponent,
     LoginComponent,
+    CadastrarIdeiaComponent,
   ],
   imports: [
     BrowserModule,
