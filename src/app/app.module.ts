0fbdd42de45c785cfdc866a00767d88ef6733af8
Rodolfo.Bandeira <rodolfobandeira@id.uff.br>
2021-04-20 20:50:11 -0300

    Merge branch Milena

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
