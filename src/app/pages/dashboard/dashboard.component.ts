8770c8b1322ff6e17bd24656f61d4481ac9926b3
Rodolfo.Bandeira <rodolfobandeira@id.uff.br>
2021-05-02 01:04:16 -0300

    Refatoração das busca por ideias no dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,44 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Ideia } from 'src/app/shared/ideia/ideia';
+import { IdeiaService } from 'src/app/shared/ideia/ideia.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['../../../assets/stylesheets/dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  readonly apiURL : string;
-  result : Array<any>;
+
+  //readonly apiURL : string;
+  result!: Array<Ideia>;
 
   constructor(
     private http : HttpClient,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private ideiaService: IdeiaService
     ) {
-    this.apiURL = 'https://plugue.herokuapp.com/';
-    this.result = [];
   }
 
   ngOnInit(): void {
-    this.listIdeas();
     const idUsuario = this.route.snapshot.paramMap.get('id');
     const tipoUsuario = this.route.snapshot.paramMap.get('tipoUsuario');
-    console.log(idUsuario);
+    this.listaIdeias();
+    this.result = [];
   }
 
-  listIdeas() {
-    const headers = new HttpHeaders({
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
-      "Content-Type": "application/json"
-    });
-
-    return this.http.get(`${this.apiURL}ideia`, { headers }).toPromise().then(ideias => {
-      const objectArray = Object.entries(ideias);
-      objectArray.forEach(([ key, value ]) => {
-        this.result.push(value);
-      })
-      //console.log(this.result)
+  listaIdeias() {
+    this.ideiaService.listarIdeias().subscribe(ideias => {
+      ideias.forEach(i => {
+        this.result.push(i)
+      });
     });
   }
 
--- src/app/shared/ideia/ideia.service.ts
+++ src/app/shared/ideia/ideia.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ideia } from './ideia';
@@ -12,16 +12,71 @@ export class IdeiaService {
     private httpClient: HttpClient
   ) { }
 
-  //path: string = 'http://127.0.0.1:8080/ideia';
-  path: string = 'https://plugue.herokuapp.com/ideia';
+  path: string = 'http://127.0.0.1:8080/ideia';
+  //path: string = 'https://plugue.herokuapp.com/ideia';
+  result!: Array<Ideia>
 
   //Headers
-  httpOptions = { headers: new HttpHeaders({ "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
-    "Content-Type": "application/json" })};
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+      "Content-Type": "application/json"
+    })
+  };
 
   salvaIdeia(ideia: Ideia): Observable<Ideia> {
     return this.httpClient.post<Ideia>(this.path, JSON.stringify(ideia), this.httpOptions);
+  }
+
+  //Lista todas as ideias
+  listarIdeias() {
+    return this.httpClient.get<Array<Ideia>>(this.path);
+  }
+
+  //Busca ideias por parâmetros
+  buscarIdeias(titulo: string, area: string) {
+    const headers = new HttpHeaders({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+      "Content-Type": "application/json"
+    });
+
+    let params = new HttpParams();
+    if (titulo) params.set('titulo', titulo);
+    if (area) params.set('area', area);
+
+
+    return this.httpClient.get(`${this.path}`, { headers, params }).toPromise().then(ideias => {
+      const objectArray = Object.entries(ideias);
+      objectArray.forEach(([key, value]) => {
+        this.result.push(value);
+      })
+      console.log(this.result)
+    });
+  }
+
+  // Interessar por ideia
+  applyIdea() {
+    const headers = new HttpHeaders({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+      "Content-Type": "application/json"
+    });
+
+    this.httpClient.put(`${this.path}/${"teste"}/ideia/${"38"}`, {}, { headers }).subscribe(
+      val => {
+        console.log("PUT call successful value returned in body", val);
+      },
+      response => {
+        console.log("PUT call in error", response);
+      },
+      () => {
+        console.log("The PUT observable is now completed.");
+      }
+    );
   }
 }
