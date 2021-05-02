97e3c6a0fce0d52682e7fb84db60fa5d212d09cc
Gleison Lima <gleisonlima@id.uff.br>
2021-05-02 01:26:34 -0300

    Repositório de ideias

--- src/app/pages/repositorio-de-ideias/repositorio-de-ideias.component.ts
+++ src/app/pages/repositorio-de-ideias/repositorio-de-ideias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-repositorio-de-ideias',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositorioDeIdeiasComponent implements OnInit {
 
-  constructor() { }
+  readonly apiURL : string;
+  result : Array<any>;
+
+  constructor(private http : HttpClient) {
+    this.apiURL = 'https://plugue.herokuapp.com/';
+    this.result = [];
+  }
 
   ngOnInit(): void {
+    this.listIdeas();
+  }
+
+  listIdeas() {
+    const headers = new HttpHeaders({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+      "Content-Type": "application/json"
+    });
+
+    return this.http.get(`${this.apiURL}ideia`, { headers }).toPromise().then(ideias => {
+      const objectArray = Object.entries(ideias);
+      objectArray.forEach(([ key, value ]) => {
+        this.result.push(value);
+      })
+      console.log(this.result)
+    });
   }
 
 }
