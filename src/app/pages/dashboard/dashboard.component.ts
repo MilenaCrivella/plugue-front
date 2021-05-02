0b56aa85f5d9c3acab3af334c3e7ed24ed73faac
Rodolfo.Bandeira <rodolfobandeira@id.uff.br>
2021-05-01 23:44:53 -0300

    Variável de sessão do login

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,13 +10,19 @@ export class DashboardComponent implements OnInit {
   readonly apiURL : string;
   result : Array<any>;
 
-  constructor(private http : HttpClient) {
+  constructor(
+    private http : HttpClient,
+    private route: ActivatedRoute
+    ) {
     this.apiURL = 'https://plugue.herokuapp.com/';
     this.result = [];
   }
 
   ngOnInit(): void {
     this.listIdeas();
+    const idUsuario = this.route.snapshot.paramMap.get('id');
+    const tipoUsuario = this.route.snapshot.paramMap.get('tipoUsuario');
+    console.log(idUsuario);
   }
 
   listIdeas() {
@@ -31,7 +38,7 @@ export class DashboardComponent implements OnInit {
       objectArray.forEach(([ key, value ]) => {
         this.result.push(value);
       })
-      console.log(this.result)
+      //console.log(this.result)
     });
   }
 
